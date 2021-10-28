46f1480e3888868bb3b21d0684d5ac1820416f0e
Mahbod <mahbodnouri@gmail.com>
2021-10-28 21:33:08 +0100

    rename + update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='FastSpike',
-    version='0.0.1',
+    name="FastSpike",
+    version="0.0.1",
+    description="Spiking neural networks for ML in Python",
+    license="MIT License",
+    url="https://github.com/mahbodnr/FastSpike",
+    author="Mahbod Nouri",
+    author_email="MahbodNouri@gmail.com",
+    packages=find_packages(),
+    zip_safe=False,
     install_requires=[
-        'torch'
+        "torch"
     ],
 )
